b4d2a0cf003161c784a6f85b67a6e04a422279c6
Oliver Ulvebne <oliver.ulvebne@gmail.com>
2025-03-08 18:27:43 +0000

    removed animation from navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout() {
             <Stack
               screenOptions={{
                 headerShown: false,
+                animation: "none",
               }}
             />
           </View>
